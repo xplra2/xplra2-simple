362de45d47e09b85b10b668dd2d6ce2aca4071bc
sletap <sletap@umich.edu>
2019-06-09 04:12:40 -0400

    figured stuff out

--- vueScript.js
+++ vueScript.js
@@ -1,4 +1,5 @@
 var map;
+var MyGeocoder;
 
 // pass in 'ConstructionPin' as format for pinType
 function createPin(latLong, myMap, title, pinType){
@@ -7,18 +8,41 @@ function createPin(latLong, myMap, title, pinType){
     map: myMap,
     title: title,
     icon: './src/markers/' + pinType + '.svg',
+  }).addListener('click', function(){
   });
 }
+
+function geocodeLatLng(geocoder, latlng) {
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === 'OK') {
+      if (results[0]) {
+        console.log(results[0].formatted_address)
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
+
 
 var myLatLng = {lat: 42.2808, lng: -83.7430};
 function initMap() {
+  MyGeocoder = new google.maps.Geocoder()
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42.2808, lng: -83.7430},
     zoom: 14
-  });
+  })
 
-  createPin(myLatLng, map, "fjkladsfdjal", 'GaragePin');
+  // createPin(myLatLng, map, "fjkladsfdjal", 'GaragePin');
+  // geocodeLatLng(MyGeocoder, {lat: 42.2808, lng: -83.7430});
+  // document.getElementById.
 }
+createPin(myLatLng, map, "fjkladsfdjal", 'GaragePin');
+
+
 
 
 
